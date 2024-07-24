a39e6069106c65300276357cc71aff8b79292447
manugcampos <contactomanuelgutierrez@gmail.com>
2024-07-24 13:09:32 +0200

    chore: Update Astro configuration and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import robotsTxt from 'astro-robots-txt';
 import astroI18next from 'astro-i18next';
+import vercel from '@astrojs/vercel/serverless';
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +12,7 @@ export default defineConfig({
     tailwind(),
     robotsTxt()
   ],
-  site: 'https://porfolio.dev/'
+  site: 'https://porfolio.dev/',
+  output: 'server',
+  adapter: vercel(),
 });
