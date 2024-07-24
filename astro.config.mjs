4133be4f249066d2200d9f590c8a36cd6b73b6e6
manugcampos <contactomanuelgutierrez@gmail.com>
2024-07-24 13:05:04 +0200

    chore: Update Astro configuration and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
-
-import robotsTxt from "astro-robots-txt"
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import robotsTxt from 'astro-robots-txt';
+import astroI18next from 'astro-i18next';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
+  integrations: [
+    astroI18next(),
+    tailwind(),
+    robotsTxt()
+  ],
   site: 'https://porfolio.dev/'
-})
+});
